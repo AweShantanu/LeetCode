c11bf6ae1568ea9ecdd3e770cf790d3edea6421c
Shantanu Aditya <117399781+AweShantanu@users.noreply.github.com>
2023-09-14 16:39:10 +0530

    Time: 4 ms (31.59%), Space: 8.6 MB (13.65%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,18 +1,49 @@
-class Solution {
+// class Solution {
+// public:
+//     void sortColors(vector<int>& arr) {
+//         int n=arr.size();
+//         int c0=0,c1=0,c2=0;
+//         for(int i=0;i<n;i++){
+//             c0+=(arr[i]==0);
+//             c1+=(arr[i]==1);
+//             c2+=(arr[i]==2);
+//         }
+//         cout<<c0<<" "<<c0<<" "<<c2;
+//         for(int i=0;i<n;i++){
+//             if(c0>0)arr[i]=0,c0--;
+//             else if(c1>0)arr[i]=1,c1--;
+//             else arr[i]=2;
+//         }
+//     }
+// };
+
+//three pointers approach se bhi karna hai
+class Solution{
 public:
-    void sortColors(vector<int>& arr) {
-        int n=arr.size();
-        int c0=0,c1=0,c2=0;
-        for(int i=0;i<n;i++){
-            c0+=(arr[i]==0);
-            c1+=(arr[i]==1);
-            c2+=(arr[i]==2);
-        }
-        cout<<c0<<" "<<c0<<" "<<c2;
-        for(int i=0;i<n;i++){
-            if(c0>0)arr[i]=0,c0--;
-            else if(c1>0)arr[i]=1,c1--;
-            else arr[i]=2;
+    void sortColors(vector<int>& arr)
+    {
+        int s=0;
+        int e=arr.size()-1;
+        int mid=0;
+        while(mid<=e)
+        {
+            
+            if(arr[mid]==0)
+            {
+                swap(arr[s],arr[mid]);
+                s++;
+                mid++;   
+            }
+            else if(arr[mid]==1)
+            {
+                mid++;
+            }
+            else{
+                swap(arr[mid],arr[e]);
+                e--;
+            }
         }
     }
+    
+    
 };
