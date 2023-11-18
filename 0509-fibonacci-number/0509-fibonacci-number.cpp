f255c99a43d3c4a46d3c9360f715501d1a9459bd
Shantanu Aditya <117399781+AweShantanu@users.noreply.github.com>
2023-11-18 12:57:37 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (23.77%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    int recSolve(int n)
+    // int recSolve(int n)
+    // {
+    //     if(n==1 || n==0)
+    //         return n;
+    //     int ans=recSolve(n-1)+recSolve(n-2);
+    //     return ans;
+    // }
+    
+    //DP
+    int topDown(int n, vector<int>& dp)
     {
         if(n==1 || n==0)
             return n;
-        int ans=recSolve(n-1)+recSolve(n-2);
-        return ans;
+        
+        //step 3: check if ans is already exost
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        //step 2: replace ans with dp[n]
+        dp[n]=topDown(n-1,dp)+topDown(n-2,dp);
+        return dp[n];
     }
     
     int fib(int n) {
-        int ans=recSolve(n);
-        return ans;
+        // int ans=recSolve(n);
+        // return ans;
         
+        //step 1: create DP array
+        vector<int> dp(n+1,-1);
+        int ans=topDown(n,dp);
+        return ans;
     }
 };
