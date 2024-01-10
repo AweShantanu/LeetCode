7d4085efcee2fb3cd6be4bcadde282508bea7199
Shantanu Aditya <117399781+AweShantanu@users.noreply.github.com>
2024-01-11 00:38:54 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (24.30%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,35 +1,113 @@
+// class Solution {
+// public:
+//     int result;
+//     int n,m;
+//     int countOfObs;
+//     vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
+//     void solve(vector<vector<int>>& grid , int i, int j , int count)
+//     {
+//         if(i<0 || i>m || j<0 || j>n || grid[i][j]==-1)
+//             return ;
+//         if(grid[i][j]==2){
+//             if(count ==countOfObs)
+//             {
+//                 result++;
+//                 return ;
+//             }
+//         }
+//         grid[i][j]=-1;
+//         for(auto &dir : directions)
+//         {
+//             int new_i=i+dir[0];
+//             int new_j=j+dir[1];
+//             solve(grid,new_i,new_j,count+1);
+//         }
+//         grid[i][j]=0;
+//     }
+//     int uniquePathsIII(vector<vector<int>>& grid) {
+//         result=0;
+//         m=grid.size();
+//         n=grid[0].size();
+//         int st_i=0;
+//         int st_j=0;
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(grid[i][j]==1)
+//                 {
+//                     st_i=i;
+//                     st_j=j;
+//                 }
+//                 if(grid[i][j]==0)
+//                 {
+//                     countOfObs++;
+//                 }   
+//             }
+//         }
+//         countOfObs=+1;
+//         int count=0;
+//         solve(grid,st_i,st_j,count);
+//         return result;
+//     }
+// };
+#include <vector>
+
 class Solution {
 public:
-    int res = 0, empty = 1;
-    void dfs(vector<vector<int>>& grid, int x, int y, int count) {
-        if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] == -1) return;
-        
-        if (grid[x][y] == 2) {
-            if(empty == count) res++; 
+    int result;
+    int n, m;
+    int countOfObs;
+    std::vector<std::vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    void solve(std::vector<std::vector<int>>& grid, int i, int j, int count) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1)
+            return;
+
+        if (grid[i][j] == 2) {
+            if (count == countOfObs) {
+                result++;
+            }
             return;
         }
-        
-        grid[x][y] = -1;
-        
-        dfs(grid, x+1, y, count+1);
-        dfs(grid, x-1, y, count+1);
-        dfs(grid, x, y+1, count+1);
-        dfs(grid, x, y-1, count+1);
-        
-        grid[x][y] = 0;
-        
+
+        grid[i][j] = -1;
+
+        for (auto& dir : directions) {
+            int new_i = i + dir[0];
+            int new_j = j + dir[1];
+            solve(grid, new_i, new_j, count + 1);
+        }
+
+        grid[i][j] = 0;
     }
-    
-    int uniquePathsIII(vector<vector<int>>& grid) {
-        int start_x, start_y;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == 1) start_x = i, start_y = j;
-                else if (grid[i][j] == 0) empty++;
+
+    int uniquePathsIII(std::vector<std::vector<int>>& grid) {
+        result = 0;
+        m = grid.size();
+        n = grid[0].size();
+        int st_i = 0;
+        int st_j = 0;
+        countOfObs = 0;  // Initialize countOfObs to zero
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    st_i = i;
+                    st_j = j;
+                }
+                if (grid[i][j] == 0) {
+                    countOfObs++;
+                }
             }
         }
-        
-        dfs(grid, start_x, start_y, 0);
-        return res;
+
+        countOfObs += 1;
+
+        int count = 0;
+        solve(grid, st_i, st_j, count);
+
+        return result;
     }
 };
+
