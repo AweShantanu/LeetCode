46c19eff8fd329a2eed8287a971e97a379ae7109
Shantanu Aditya <117399781+AweShantanu@users.noreply.github.com>
2024-05-29 12:21:37 +0530

    Time: 9 ms (44.18%), Space: 15 MB (51.53%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -1,3 +1,48 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* removeZeroSumSublists(ListNode* head) {
+//         unordered_map<int, ListNode*> mp;
+//         ListNode* dummy=new ListNode(0);
+//         mp[0]=dummy;
+//         dummy->next=head;
+//         int preSum=0;
+//         while(head!=NULL)
+//         {
+//             preSum=preSum+head->val;
+//             if(mp.find(preSum)!=mp.end())
+//             {
+//                 ListNode* as=mp[preSum];
+//                 ListNode* ass=as;
+//                 int pSum=preSum;
+//                 while(as!=head)
+//                 {
+//                     as=as->next;
+//                     pSum=pSum+as->val;
+//                     if(as!=NULL)
+//                         mp.erase(pSum);
+//                 }
+//                 ass->next=head->next;
+                
+//             }
+//             else{
+//                 mp[preSum]=head;
+//             }
+//             head=head->next;
+//         }
+//         return dummy->next;
+//     }
+// };
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,33 +57,29 @@ class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
         unordered_map<int, ListNode*> mp;
-        ListNode* dummy=new ListNode(0);
-        mp[0]=dummy;
-        dummy->next=head;
-        int preSum=0;
-        while(head!=NULL)
-        {
-            preSum=preSum+head->val;
-            if(mp.find(preSum)!=mp.end())
-            {
-                ListNode* as=mp[preSum];
-                ListNode* ass=as;
-                int pSum=preSum;
-                while(as!=head)
-                {
-                    as=as->next;
-                    pSum=pSum+as->val;
-                    if(as!=head)
-                        mp.erase(pSum);
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        mp[0] = dummy;
+
+        int preSum = 0;
+        while (head != NULL) {
+            preSum += head->val;
+            if (mp.find(preSum) != mp.end()) {
+                ListNode* start = mp[preSum];
+                ListNode* current = start->next;
+                int sum = preSum;
+                while (current != head) {
+                    sum += current->val;
+                    mp.erase(sum);
+                    current = current->next;
                 }
-                ass->next=head->next;
-                
+                start->next = head->next;
+            } else {
+                mp[preSum] = head;
             }
-            else{
-                mp[preSum]=head;
-            }
-            head=head->next;
+            head = head->next;
         }
+
         return dummy->next;
     }
 };
