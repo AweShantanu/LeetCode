9255322498f07077dfe551dc7288a9b48cbedde1
Shantanu Aditya <117399781+AweShantanu@users.noreply.github.com>
2024-06-01 18:06:08 +0530

    Time: 159 ms (66.84%), Space: 97.6 MB (34.49%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n=nums1.size();
+        vector<pair<int ,int>> vec(n);
+        for(int i=0;i<n;i++)
+        {
+            vec.push_back({nums1[i],nums2[i]});
+        }
+        
+        auto lambda=[&](auto &p1,auto &p2)
+        {
+            return p1.second > p2.second;
+        };
+        sort(begin(vec),end(vec),lambda); //desending order mein sort kar rahen hain
+         
+        priority_queue<int,vector<int>,greater<int>> pq;
+        long long kSum=0;
+        for(int i=0;i<=k-1;i++)
+        {
+            kSum=kSum+vec[i].first;
+            pq.push(vec[i].first);
+            
+        }
+        long long res=kSum*vec[k-1].second;
+        for(int i=k;i<n;i++)
+        {
+            kSum+=vec[i].first-pq.top();
+            pq.pop();
+            pq.push(vec[i].first);
+            res=max(res,kSum*vec[i].second);
+        }
+        
+        return res;
+        
+        
+        
+        
+    }
+};
