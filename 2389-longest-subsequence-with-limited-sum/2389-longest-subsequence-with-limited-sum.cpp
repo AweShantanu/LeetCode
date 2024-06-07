0b641042a80f3b5860c81b4e4999555ff09121d1
Shantanu Aditya <117399781+AweShantanu@users.noreply.github.com>
2024-06-07 07:38:25 +0530

    Time: 23 ms (18.93%), Space: 119.8 MB (5.89%) - LeetHub

---
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> ans;
+    int binarySearch(vector<int> cumSum,int target,int n)
+    {
+        int l=0;int r=n-1;
+        int resIndex=-1;
+        int mid;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(cumSum[mid]<=target)
+            {
+                 resIndex=mid;
+                l=mid+1;
+            }   
+            else{
+                r=mid-1;
+            }
+            
+            
+        }
+        return resIndex+1;
+    }
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<int> cumSum;
+        int m=queries.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+nums[i];
+            cumSum.push_back(sum);
+            
+        }
+        for(int i=0;i<m;i++)
+        {
+            int k= binarySearch(cumSum,queries[i],n);
+            ans.push_back(k);
+        }
+        return ans;
+        
+        
+    }
+};
