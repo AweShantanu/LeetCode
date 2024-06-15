7157fa335c59b097439a69537f3c5909e7853483
Shantanu Aditya <117399781+AweShantanu@users.noreply.github.com>
2024-06-15 09:08:36 +0530

    Time: 26 ms (24.17%), Space: 13.5 MB (27.43%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,45 +1,41 @@
+#include <string>
+#include <algorithm>
+
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        int open=0;
-        int n=s.size();
-        string res="";
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='(')
-            {
-                
+        int open = 0;
+        int n = s.size();
+        string res = "";
+
+        // First pass: remove unmatched closing brackets
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
                 open++;
                 res.push_back(s[i]);
-            }
-            else if(s[i]==')')
-            {
-                if(open>0)
-                {
+            } else if (s[i] == ')') {
+                if (open > 0) {
                     open--;
-                res.push_back(s[i]);
+                    res.push_back(s[i]);
                 }
-                
-            }
-            else{
+            } else {
                 res.push_back(s[i]);
             }
         }
-        //ab ham ye dekh rahe hain ki agar open ka count kuch bhi bacha hai to ham ab wo extra open bracket ko nikalenge 
-        string result="";
-        int m=res.size();
-        for(int i=m-1;i>=0;i--)
-        {
-            if(res[i]=='(' && open >0)
-            {
+
+        // Second pass: remove unmatched opening brackets from the end
+        string result = "";
+        int m = res.size();
+        for (int i = m - 1; i >= 0; i--) {
+            if (res[i] == '(' && open > 0) {
                 open--;
-                
-            }
-            else{
+            } else {
                 result.push_back(res[i]);
             }
         }
-        reverse(result.begin(),result.end());
+
+        // Reverse the result to restore the correct order
+        reverse(result.begin(), result.end());
         return result;
     }
 };
